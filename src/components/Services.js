0c976a826359636028062319d85d5af31d1f0c73
martinchez <martinchege1998@gmail.com>
2023-06-09 17:36:50 +0300

    finalizing on the data setup for  the tours

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,48 +1,28 @@
 import Title from './title'
+import {services} from '../data'
 const Services = () => {
   return (
     <section className="section services" id="services">
       <Title title="Our" subTitle="services" />
-      
+
       <div className="section-center services-center">
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-wallet fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">saving money</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-tree fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">endless hiking</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-socks fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">amazing comfort</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
+        {services.map((service) => {
+          const { id, title, text, icon } = service
+          return (
+            <article key ={id} className="service">
+              <span className="service-icon">
+                <i className={icon}></i>
+              </span>
+              <div className="service-info">
+                <h4 className="service-title">{title}</h4>
+                <p className="service-text">
+                 {text}
+                </p>
+              </div>
+            </article>
+          )
+        })}
+        
       </div>
     </section>
   )
--- src/data.js
+++ src/data.js
@@ -1,3 +1,9 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+import tour5 from './images/tour-5.jpeg'
+import tour6 from './images/tour-6.jpeg'
 export const pageLinks = [
   { id: 1, href: '#home', text: 'home' },
   { id: 2, href: '#about', text: 'about' },
@@ -20,5 +26,74 @@ export const socialLinks = [
     id: 3,
     href: 'https://www.instagram.com/anmartdevs',
     icon: 'fab fa-instagram',
+  },
+]
+// handling data for the 
+export const services = [
+    { id: 1, icon: 'fas fa-wallet fa-fw' ,title:"Game Drive", text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."},
+    { id: 2, icon: 'fas fa-tree fa-fw', title:"Forest Trails", text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."},
+    { id: 3, icon: 'fas fa-wallet fa-fw', title:"Water Falls", text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."},
+ ]
+//  handling Tours Data
+export const tours = [
+  {
+    id: 1,
+    img: tour1,
+    tDate: 'august 26th, 2020',
+    tTitle: 'Archives',
+    text: 'lorem ipsum dolor sit amet, consectetur adip',
+    state: 'Town',
+    days: '6 days',
+    price: '2100',
+  },
+  {
+    id: 2,
+    img: tour2,
+    tDate: 'august 25th, 2020',
+    tTitle: 'chania falls',
+    text: 'lorem ipsum dolor sit amet',
+    state: 'Mathira',
+    days: '6 days',
+    price: '2100',
+  },
+  {
+    id: 3,
+    img: tour3,
+    tDate: 'august 27th, 2020',
+    tTitle: 'samak dam',
+    text: 'lorem ipsum dolor sit amet, con',
+    state: 'Nyeri',
+    days: '6 days',
+    price: '2100',
+  },
+  {
+    id: 4,
+    img: tour4,
+    tDate: 'august 27th, 2020',
+    tTitle: 'chnga dams',
+    text: 'lorem ipsum dolor sit amet',
+    state: 'Nyeri',
+    days: '6 days',
+    price: '100',
+  },
+  {
+    id: 5,
+    img: tour5,
+    tDate: 'august 26th, 2020',
+    tTitle: 'ZXaina Falls',
+    text: 'lorem ipsum lorem ipsum',
+    state: 'KImathi',
+    days: '6 days',
+    price: '2100',
+  },
+  {
+    id: 5,
+    img: tour6,
+    tDate: 'august 26th, 2020',
+    tTitle: 'RangeLands',
+    text: 'lorem ipsum',
+    state: 'Chieni',
+    days: '6 days',
+    price: '2100',
   },
 ]
